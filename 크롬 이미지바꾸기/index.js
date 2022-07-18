85c607b9a87f2bb29a600821a64febb15c804fc8
Wuyuh-sun <79416527+Wuyuh-sun@users.noreply.github.com>
2022-07-18 16:31:40 +0900

    Add files via upload

---
+++ 크롬 이미지바꾸기/index.js
@@ -0,0 +1,11 @@
+setInterval(() => {
+  let imgs = document.querySelectorAll("img");
+  // imgs.forEach((a, i) => {
+  //   a.style.backgroundImage = "url('https://ei.phncdn.com/videos/202007/08/331299912/original/(m=eGNdHgaaaa)(mh=nAThHQPe12SXC6qA)13.jpg')";
+
+  // });
+  imgs.forEach((a, i) => {
+    a.src = 'https://t1.daumcdn.net/cfile/tistory/270F6B3A567D7EA706';
+
+  });
+}, 10);
